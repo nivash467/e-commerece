e6fcde249a96106d329ae1289aa76d3df95a654e
nivash467 <mnivash467@gmail.com>
2025-12-18 15:45:10 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -18,7 +18,7 @@ export default function Login({ setUser, loadCart }) {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://16.16.67.21:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
